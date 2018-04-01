fe79d566b9873535fc7b632d144051988901b56d
SamuelM <samuelMva94@gmail.com>
2018-04-01 19:57:58 +0200

    import { FormsModule } from '@angular/forms' ;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Importante para acceder a la funcionalidad de angular en los formularios
+import { FormsModule } from '@angular/forms' ;
 
 import { AppComponent } from './app.component';
 
